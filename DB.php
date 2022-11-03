af0ad01b137ade9db5e101dc87ffacdafe83a9ea
reem1alshareef <441200087@student.ksu.edu.sa>
2022-11-04 00:20:36 +0300

    ز

--- DB.php
+++
@@ -1,9 +0,0 @@
-<?php
-$serverName="localhost";
-$DBUserName="root";
-$DBpasswprd="";
-$DBName="webproject";
- $conn = mysqli_connect($serverName,$DBUserName,$DBpasswprd,$DBName);
- if (!$conn) 
-die("Connection failed: " . mysqli_connect_error());
-
